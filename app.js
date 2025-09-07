6e9ac4167154ef313f5456ad3e7271f6a6b6c8ba
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-07 10:58:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ let faveCourse = 'Web Development';
 let coursePrice = 15;
 let goals = ['Learn programming', 'Become a developer', 'Get a job'];
 
-alert(faveCourse);
-alert(coursePrice);
-alert(goals);
+let variables = [faveCourse, coursePrice, goals];
+
+alert(variables);
