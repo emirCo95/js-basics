239c69d72a64d7b14621c6ddf557d9ca0cfd1534
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-07 12:19:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-let faveCourse = 'Web Development';
-let coursePrice = 15;
-let goals = ['Learn programming', 'Become a developer', 'Get a job'];
+// let faveCourse = 'Web Development';
+// let coursePrice = 15;
+// let goals = ['Learn programming', 'Become a developer', 'Get a job'];
 
-let variables = [faveCourse, coursePrice, goals];
+// let variables = [faveCourse, coursePrice, goals];
 
-// alert(variables);
-// alert(goals[1]);
+// // alert(variables);
+// // alert(goals[1]);
 
-function findGoal(identifier, goals) {
-  return goals[identifier];
-}
+// function findGoal(identifier, goals) {
+//   return goals[identifier];
+// }
 
-alert(findGoal(0, goals));
+// alert(findGoal(0, goals));
+
+let person = {
+  name: 'Seamus', //property
+  greet() {
+    // method
+    alert('Hello!!!');
+  },
+};
