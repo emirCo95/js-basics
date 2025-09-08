c0da0f1b3d1897469f70df6dc1d453e853b5b95f
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-08 16:36:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-document.getElementById('external-link');
+let anchorElement = document.getElementById('external-link');
+
+anchorElement.href = 'https://google.com';
+
+console.log(anchorElement.href);
+
+anchorElement = document.querySelector('#external-link');
+anchorElement.href = 'https://academind.com';
