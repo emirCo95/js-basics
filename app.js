c9afa5401e16722aa4d78141436fffcbe58a9c48
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-08 16:51:28 -0400

    updated both files

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-let anchorElement = document.getElementById('external-link');
-
-anchorElement.href = 'https://google.com';
-
-console.log(anchorElement.href);
-
-anchorElement = document.querySelector('#external-link');
-anchorElement.href = 'https://academind.com';
+// ADD AN ELEMENT
+//1. CREATE THE NEW ELEMENT
+let newAnchorEl = document.createElement('a');
+newAnchorEl.href = 'https://google.com';
+newAnchorEl.textContent = 'This leads to google!';
+//2. GET ACCESS TO THE PARENT ELEMENT THAT SHOULD HOLD THE NEW ELEMENT
+let firstParagraph = document.querySelector('p');
+//3. INSERT THE NEW ELEMENT INTO THE PARENT ELEMENT CONTENT
+firstParagraph.appendChild(newAnchorEl);
