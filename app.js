3a8fee66b11a5b847df136556378c9ec999c9f25
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-09-08 20:08:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ newAnchorEl.textContent = 'This leads to google!';
 let firstParagraph = document.querySelector('p');
 //3. INSERT THE NEW ELEMENT INTO THE PARENT ELEMENT CONTENT
 firstParagraph.appendChild(newAnchorEl);
+// REMOVE ELEMENTS
+//1.  SELECT THE ELEMENT THAT SHOULD BE REMOVED
+let firstH1Element = document.querySelector('h1');
+//2. RFEMOVE IT
+firstH1Element.remove();
+firstH1Element.parentElement.removeChild(firstH1Element);
+
+//MOVE ELEMENTS
